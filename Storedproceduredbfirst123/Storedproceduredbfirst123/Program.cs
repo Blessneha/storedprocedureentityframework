5ac32ad9f8c407695f0f0c74db3fbd4c34539ff4
Blessneha <91933800+Blessneha@users.noreply.github.com>
2022-09-12 23:19:29 +0530

    Add files via upload

---
+++ Storedproceduredbfirst123/Storedproceduredbfirst123/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storedproceduredbfirst123
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                assign2Entities1 db=new assign2Entities1();
+                List<getallemployees_Result> employee = db.getallemployees().ToList();
+                foreach (var r in employee)
+                {
+                    Console.WriteLine($"empid: {r.empid},employeename: {r.empname}");
+                }
+                //  db.Addemployees(8, "hellen");
+                //  db.Addemployees(9, "hazel");
+                db.Deleteemployee(3);
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);  
+            }
+        }
+    }
+}
